1bb5903c39664394bdb8b2b2b0f6be082808f4aa
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2025-05-13 16:48:50 +0300

    first commit

---
+++ LoginFrame.java
@@ -0,0 +1,9 @@
+
+public class LoginFrame extends JFrame{
+	private JTextField nameField;
+	private JPasswordFiel passwordField;
+	public LoginFrame(){
+		super("Login");
+		setLayout(new FlowLayout());
+	}
+}
