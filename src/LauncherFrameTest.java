49b85709c1a2c5760533a0f22c5be661bb901501
erayilmazz <erayilmaz70@gmail.com>
2025-05-17 22:46:10 +0300

    LauncherFrame created.

---
+++ src/LauncherFrameTest.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class LauncherFrameTest {
+
+	public static void main(String[] args) {
+		LauncherFrame launcherFrame = new LauncherFrame();
+		launcherFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		launcherFrame.setSize(400, 300);
+		launcherFrame.setVisible(true);
+
+	}
+
+}
+
